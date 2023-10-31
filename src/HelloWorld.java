df4365bcd34dc242cc4f408d1ee5188f892e2e0c
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-10-31 11:42:11 -0500

    java helloworld intro

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        String animal = "Dog";
+        String breed = "ShibaDoodle";
+        int weight = 70;
+        boolean cuteOrNot = false;
+        cuteOrNot = true;
+        String name = "Monster Turtle";
+        System.out.println(name + " is a " + breed);
+
+        if (cuteOrNot) {
+            System.out.println(name + " is cute ");
+        } else {
+            System.out.println(name + " is mean looking");
+        }
+    }
+}

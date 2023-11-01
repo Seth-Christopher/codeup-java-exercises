b2d90a404a150258ecfb6a2c835385e8181d74fc
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-11-01 10:20:15 -0500

    completed syntax, types, and variables java exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -10,17 +10,52 @@ public class ConsoleExercises {
         System.out.printf("You %s's, welcome to week %d !!! %nYou are %.4f%% of the way through.", cohort, week, completionPercentage);
         System.out.printf("%nThis is a new line.%n");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String userInput = scanner.nextLine();
-        System.out.printf("Well hello %s , you cheeky bastard ;)", userInput);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What is your name?");
+//        String userInput = scanner.nextLine();
+//        System.out.printf("Well hello %s , you cheeky bastard ;)", userInput);
+//
+//
+//        System.out.printf("%n Now enter a number:");
+//        int userNumber = scanner.nextInt();
+//        System.out.printf("You entered %d%n", userNumber);
+//        System.out.printf("%n Are you sure?%n");
 
 
-        System.out.printf("%n Now enter a number:");
-        int userNumber = scanner.nextInt();
-        System.out.printf("You entered %d%n", userNumber);
-        System.out.printf("%n Are you sure?");
+        String myString = "Go Birds!";
+        int myFavNumber = 2;
+        float myNumber = 123.654F;
+        System.out.printf("%d,%s" , myFavNumber, myString);
 
 
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        /* Post-Increment (i++): we use i++ in our statement if we want to use the current value, and then we want to increment the value of i by 1. 2) Pre-Increment(++i): We use ++i in our statement if we want to increment the value of i by 1 and then use it in our statement */
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+//
+
+
+// Rewrite the following expressions using the relevant shorthand assignment operators:
+//        int x = 4;
+////        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+//        [^1]: How many bytes of memory are allocated to store a variable of this data type
+        // 1?
     }
 }

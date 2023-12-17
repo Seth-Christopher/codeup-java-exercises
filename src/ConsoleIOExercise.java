1970de9fc0fa6c9c97da3c01e654da4e110a0ed3
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-12-16 23:40:59 -0600

    restarting all of Java I.

--- src/ConsoleIOExercise.java
+++ src/ConsoleIOExercise.java
@@ -3,44 +3,61 @@ import java.util.Scanner;
 
 public class ConsoleIOExercise {
     public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %.2f.%n", pi);
+//
+        Scanner userPrompt = new Scanner(System.in);
+////        System.out.println("Enter a number");
+////        String userInput = userPrompt.nextLine();
+//
+//        Scanner userPrompt2 = new Scanner(System.in);
+//        String string1;
+//        String string2;
+//        String string3;
+//        System.out.println("Enter a three words:");
+//
+//        string1 = userPrompt2.nextLine();
+//        string2 = userPrompt2.nextLine();
+//        string3 = userPrompt2.nextLine();
+//
+//        System.out.printf("You entered %s, %s, %s %n" , string1,string2,string3 );
+//
+//        String fullSentence;
+//        System.out.println("Write me a sentence:");
+//
+//        fullSentence = userPrompt2.nextLine();
+//        System.out.println("Your Sentence: " + fullSentence);
+//
+//        int length;
+//        int width;
+//        int perimeter;
+//        int area;
+//        System.out.println("What is the length of your living room?");
+//        length = Integer.parseInt(userPrompt2.nextLine());
+//        System.out.println("What is the width in feet of your living room?");
+//        width = Integer.parseInt(userPrompt2.nextLine());
+//        area = length * width;
+//        perimeter = 2*length + 2*width;
+//
+//        System.out.printf("The area of your living room is %dft , and the perimeter of your living room is %dft.", area, perimeter);
+
+
+        // THE ABOVE CODE IS MY INITIAL WORK. BELOW, IS JUST MY REFRESHER
+
         double pi = 3.14159;
-        System.out.format("The value of pi is approximately %.2f.%n", pi);
 
-//        Scanner userPrompt = new Scanner(System.in);
-//        System.out.println("Enter a number");
-//        String userInput = userPrompt.nextLine();
+        System.out.printf("The value of pi is approximately %.2f. %n", pi);
 
-        Scanner userPrompt2 = new Scanner(System.in);
-        String string1;
-        String string2;
-        String string3;
-        System.out.println("Enter a three words:");
+        System.out.println("Quick, give me a number, any number.");
+        String anyNumber = userPrompt.nextLine();
 
-        string1 = userPrompt2.nextLine();
-        string2 = userPrompt2.nextLine();
-        string3 = userPrompt2.nextLine();
+        System.out.printf("So the number you choose is %s ?", anyNumber);
 
-        System.out.printf("You entered %s, %s, %s %n" , string1,string2,string3 );
 
-        String fullSentence;
-        System.out.println("Write me a sentence:");
 
-        fullSentence = userPrompt2.nextLine();
-        System.out.println("Your Sentence: " + fullSentence);
-
-        int length;
-        int width;
-        int perimeter;
-        int area;
-        System.out.println("What is the length of your living room?");
-        length = Integer.parseInt(userPrompt2.nextLine());
-        System.out.println("What is the width in feet of your living room?");
-        width = Integer.parseInt(userPrompt2.nextLine());
-        area = length * width;
-        perimeter = 2*length + 2*width;
-
-        System.out.printf("The area of your living room is %dft , and the perimeter of your living room is %dft.", area, perimeter);
 
 
     }
+
+
 }

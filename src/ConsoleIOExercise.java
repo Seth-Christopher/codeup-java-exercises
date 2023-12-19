d860c4140df5b694139b15ff8e1d147b2b4eaec1
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-12-19 13:00:50 -0600

    completed consoleIOExercises second time

--- src/ConsoleIOExercise.java
+++ src/ConsoleIOExercise.java
@@ -14,8 +14,8 @@ public class ConsoleIOExercise {
 //        String string1;
 //        String string2;
 //        String string3;
-//        System.out.println("Enter a three words:");
-//
+//        System.out.println("Enter three words:");
+////
 //        string1 = userPrompt2.nextLine();
 //        string2 = userPrompt2.nextLine();
 //        string3 = userPrompt2.nextLine();
@@ -42,12 +42,14 @@ public class ConsoleIOExercise {
 //        System.out.printf("The area of your living room is %dft , and the perimeter of your living room is %dft.", area, perimeter);
 
 
-        // THE ABOVE CODE IS MY INITIAL WORK. BELOW, IS JUST MY REFRESHER
+//-------------------- THE ABOVE CODE IS MY INITIAL WORK. BELOW, IS JUST MY REFRESHER-----------
 
 //        double pi = 3.14159;
 //
 //            System.out.printf("The value of pi is approximately %.2f. %n", pi);
 //
+
+
 
 
             System.out.println("Quick, give me a number, any number.");
@@ -67,27 +69,76 @@ public class ConsoleIOExercise {
 
 
         Scanner confirmation = new Scanner(System.in);
-//        int userReplyTwo;
+
             System.out.printf("So the number you choose is %s ? %n", anyNumber);
             String userReply = confirmation.nextLine();
 
-            if (userReply.equalsIgnoreCase("Y") || userReply.equalsIgnoreCase("Yes")) {
-                System.out.println("Cool number brooo.");
-            } else {
-                System.out.println("Well that's not cool, pick again: ");
-                int userReplyTwo = confirmation.nextInt();
-                    if (!confirmation.hasNextInt()) {
-                        System.out.println("Try again, that's not a number:");
-                        confirmation.next();
-                    }
-                    userReplyTwo = confirmation.nextInt();
-                    if (userReplyTwo < 5) {
-                        System.out.println("Please make it an integer larger than 5:");
-                        confirmation.next();
-                    } else {
 
-                        System.out.println("thanks dude");
-                    }
+        int userReplyTwo;
+        if (userReply.equalsIgnoreCase("Y") || userReply.equalsIgnoreCase("Yes")) {
+                System.out.println("You win!");
+            } else {
+            System.out.println("Well that's not cool, pick again: ");
+            while (true) {
+                while (!confirmation.hasNextInt()) {
+                    System.out.println("Try again, that's not a number:");
+                    confirmation.next();
+                }
+                userReplyTwo = confirmation.nextInt();
+                if (userReplyTwo < 5) {
+                    System.out.println("Please make it an integer larger than 5:");
+                } else {
+                    System.out.println("thanks dude");
+                    break;
                 }
             }
+        }
+
+
+
+// Prompt a user to enter 3 words, each stored in a variable.
+// Using .split() method in order to grab 1 word, if the user were to input
+// multiple words on one line.
+
+        Scanner threeWords = new Scanner(System.in);
+
+        String firstWord;
+        String secondWord;
+        String thirdWord;
+        System.out.println("Enter three words: ");
+
+        firstWord = threeWords.nextLine().split(" ") [0]; // Here I use the split() method to grab ONLY the first word if the user inputs multiple words on one line.
+
+        secondWord = threeWords.nextLine().split(" ") [0]; // you can change the index depending on which word in the users given string you want to grab.
+        thirdWord = threeWords.nextLine().split(" ") [0];
+
+        System.out.printf("So %s, %s, and %s are your three words. %n", firstWord,secondWord,thirdWord);
+
+
+// --------------prompt the user to input a sentence
+
+        String userSentence;
+        System.out.println("Now enter a sentence");
+
+        userSentence = threeWords.nextLine();
+        System.out.printf("A COPY OF WHAT YOU SAID: '%s'%n", userSentence);
+
+//----------------------------- calculate and bonus
+
+        Scanner calculate = new Scanner(System.in);
+
+        calculate.useDelimiter("\n");
+        double length;
+        double width;
+
+        System.out.printf("To calculate the area and perimeter of a given area, please input a valid Length: %n");
+        length = calculate.nextDouble();
+
+        System.out.printf("Now enter a valid Width: %n");
+        width = calculate.nextDouble();
+
+        System.out.printf("Based on the Length and Width you have given me, the Area would calculate to %.3f sq.ft. %n", length * width);
+
+        System.out.printf("The Perimeter would calculate to %.3f sq.ft. %n", 2 * (length + width));
+    }
 }

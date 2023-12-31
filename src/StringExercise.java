ca15d87cfe743d8408c75f0eff36763ef0125734
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-12-30 23:56:06 -0600

    refreshing

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -70,8 +70,48 @@ public class StringExercise {
         // the following methods can be used to manipulate strings \\
 
 
+
+//          SYNTAX:  char charAt (int index);
         // returns the character at the specified index of the string.
-//        char charAt (int index);
+        // throws StringIndexOutOfBoundsException if the index is a negative value
+        // or greater than or equal to the string length.
+
+        String name = "example";
+        char ch = name.charAt(4);
+        System.out.println(ch);     // returns the char value at the 4th index
+
+//        Accessing First and Last Character by Using the charAt() Method
+        String str = "Examples of charAt method";
+        int strLength = str.length();
+        System.out.println("Character at 0 index is: " + str.charAt(0));    // fetching first character
+
+        System.out.println("Character at last index is: " + str.charAt(strLength-1)); // the last character is present at the string length-1 index
+
+
+//        Print Characters Presented at Odd Positions by Using the charAt() Method
+
+        String strTwo = "We are accessing all elements at odd index";
+        for (int i = 0; i <= strTwo.length()-1; i++) { // the -1 is necessary because array & string indices in Java start at 0, so last index of a string is always one less than its length.
+            // -1 adjusts the loop to loop over the elements of the string from the first index(0) to the last index (one less than the length of the string), avoiding an IndexOutOfBoundsException
+            if(i % 2 != 0) {
+                System.out.println("Char at index " + i + " is " + strTwo.charAt(i));
+            }
+        }
+
+//        Counting Frequency of a character in a String by Using the charAt() Method
+
+        String strThree = "Counting frequency of a character in a string";
+        int count = 0;
+        for (int i = 0; i < strThree.length()-1; i++) {
+            if (strThree.charAt(i) == 'a') {    // if statement checks if the current character is equal to 'a'. If it is,
+                count++;    // count++ increments the value of count by 1
+            }
+        }
+        System.out.println("Frequency of a is: " + count);
+
+
+
+
 
         // returns the index of the first occurrence of a certain substring
         // returns -1 if the substring is not found

fc69463497e733cc89d1ac3864f5b8576c7120f3
Seth Martinez <sethchristopher.mtz@gmail.com>
2024-01-10 23:45:52 -0600

    int length methods

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,8 +1,33 @@
 public class StringExercise {
-    public static void main(String[] args){
+
+    // ---- Integer Length Example using While loop ---- \\
 
 
+        // method to find the number of digits present in the number n
+        public int countDig(int n) {
+            int count = 0;
+            while(n != 0) {
+            // removing the last digit of the number n
+                n = n/10;
+            // increasing count by 1
+                count = count +1;
+            }
+            return count;
+        }
 
+
+    public static void main(String[] args) {
+
+        // input array
+            int arr[] = {78, 9, 2345, 899009, 1, 414, 34, 1000, 2749};
+        // size of the input array
+            int size = arr.length;
+        // creating an object of the class StringExercise
+            StringExercise obj = new StringExercise();
+                for (int i = 0; i < size; i++) {
+            int count = obj.countDig(arr[i]);
+                    System.out.println("The length of the number " + arr[i] + " is " + count);
+        }
 
         String s1 = "We don't need no education";
         String s2 = "We don't need no thought control";
@@ -18,8 +43,7 @@ public class StringExercise {
         System.out.println(s5);
 
 
-
-                //-- STRING LITERAL COMPARISON -- \\
+        //-- STRING LITERAL COMPARISON -- \\
         // the following example will print "Strings are equal"
 
 
@@ -70,7 +94,6 @@ public class StringExercise {
         // the following methods can be used to manipulate strings \\
 
 
-
 //   --------       SYNTAX:  char charAt (int index);
         // returns the character at the specified index of the string.
         // throws StringIndexOutOfBoundsException if the index is a negative value
@@ -86,34 +109,30 @@ public class StringExercise {
         int strLength = str.length();
         System.out.println("Character at 0 index is: " + str.charAt(0));    // fetching first character
 
-        System.out.println("Character at last index is: " + str.charAt(strLength-1)); // the last character is present at the string length-1 index
-
+        System.out.println("Character at last index is: " + str.charAt(strLength - 1)); // the last character is present at the string length-1 index
 
 
 //   ---     Print Characters Presented at Odd Positions by Using the charAt() Method
 
         String strTwo = "We are accessing all elements at odd index";
-        for (int i = 0; i <= strTwo.length()-1; i++) { // the -1 is necessary because array & string indices in Java start at 0, so last index of a string is always one less than its length.
+        for (int i = 0; i <= strTwo.length() - 1; i++) { // the -1 is necessary because array & string indices in Java start at 0, so last index of a string is always one less than its length.
             // -1 adjusts the loop to loop over the elements of the string from the first index(0) to the last index (one less than the length of the string), avoiding an IndexOutOfBoundsException
-            if(i % 2 != 0) {
+            if (i % 2 != 0) {
                 System.out.println("Char at index " + i + " is " + strTwo.charAt(i));
             }
         }
-
 
 
 //  ---      Counting Frequency of a character in a String by Using the charAt() Method
 
         String strThree = "Counting frequency of a character in a string";
         int count = 0;
-        for (int i = 0; i < strThree.length()-1; i++) {
+        for (int i = 0; i < strThree.length() - 1; i++) {
             if (strThree.charAt(i) == 'a') {    // if statement checks if the current character is equal to 'a'. If it is,
                 count++;    // count++ increments the value of count by 1
             }
         }
         System.out.println("Frequency of a is: " + count);
-
-
 
 
 // ---------       int indexOf(String subString);
@@ -135,28 +154,27 @@ public class StringExercise {
 
 // ----- There are 4 types of lastIndexOf() method in Java. ------- \\
 
-     // --  int lastIndexOf(int ch); -- \\
+        // --  int lastIndexOf(int ch); -- \\
         // returns last index position for the given char value
         String exampleOne = "This is index of example"; // there are 2 's' characters in this sentence
         int index1 = exampleOne.lastIndexOf('s');  // returns the last index of 's' char value
         System.out.println(index1);                   // 6
 
 
-
-    // -- int lastIndexOf(int ch, int fromIndex); -- \\
+        // -- int lastIndexOf(int ch, int fromIndex); -- \\
         // returns the last index position for the given substring
         String exampleTwo = "This is index of example";
         int index2 = exampleTwo.lastIndexOf('s', 5);
         System.out.println(index2);
 
 
-    // -- int lastIndexOf(String substring); --\\
+        // -- int lastIndexOf(String substring); --\\
         // returns the last index position for the given substring
         String exampleThree = "This is last index of example.";
         int index3 = exampleThree.lastIndexOf("of");
         System.out.println(index3);
 
-    // -- int lastIndexOf(String substring, fromIndex); --\\
+        // -- int lastIndexOf(String substring, fromIndex); --\\
         // returns last index of given position for the given substring and from index
         String exampleFour = "This is of course the last index of example.";
         int index4 = exampleFour.lastIndexOf("of", 2);  // tries to grab the index of                                                               "of"  within the first 2 indexes.
@@ -164,7 +182,6 @@ public class StringExercise {
 
         index4 = exampleFour.lastIndexOf("of", 10);
         System.out.println(index4);     // the index of "of" within the first 10 indexes is at index 8.
-
 
 
 // --------       int length();
@@ -177,11 +194,9 @@ public class StringExercise {
         // Using Logarithm
         // Using Recursion
 // Using while loop
-     public int countDig(int n)
-        {
-         int count =0;
-         while(n != 0)
-        }
+
+
+
 
 
 
